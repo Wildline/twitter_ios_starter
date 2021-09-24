c965315df0d799c52c8a7ab6a947ff2b827a1864
Wildline <Wildlinelincifort11234@gmail.com>
2021-09-23 22:25:39 -0400

    upload project

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Wildline  Lincifort on 9/23/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileimageView: UIImageView!
+
+    @IBOutlet weak var userNameLabel: UILabel!
+
+    @IBOutlet weak var tweetsContent: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
